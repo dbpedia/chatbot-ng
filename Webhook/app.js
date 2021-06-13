a4a724b62c0156eb6f74714db62aeae3848a00d5
jayeshdesai4520 <jayeshdesai4520@gmail.com>
2021-06-13 23:51:48 +0530

    Updated code

--- Webhook/app.js
+++ Webhook/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios');
 const {WebhookClient} = require('dialogflow-fulfillment')
 const app = express()
 app.use(express.json())
@@ -15,13 +16,74 @@ app.post('/webhook', (req, res) => {
     // create intentMap for handle intent
     let intentMap = new Map();
     // add intent map 2nd parameter pass function
-    intentMap.set('webhook_test_intent',handleWebHookIntent)
+    intentMap.set('webhook_test_intent',weatherIntent)
+    intentMap.set('show_active_component_list',active_components_Intent)
+    intentMap.set('reset_list_of_components',reset_components_list_Intent)
+    intentMap.set('Deactivate_component_intent',Deactivate_component_intent)
+    intentMap.set('Activate_component_intent',Activate_component_intent)
     // now agent is handle request and pass intent map
     agent.handleRequest(intentMap)
 })
-function handleWebHookIntent(agent){
-    agent.add("Hello I am Webhook demo How are you...")
+
+var activecomponents = ["one","two"];
+
+
+
+// just for API test
+function weatherIntent(agent){ 
+    const apiKey = "23756992e06787aa9225e9b361dfcd66"
+    const city = agent.parameters.city; 
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+    return axios.get(url)
+      .then(function (response) {
+        // handle success
+        console.log(response.data); 
+        agent.add("Temp in " + city +  " is " + response.data.main.temp)
+        
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
 }
+
+
+// adding info soon in comments 
+
+function active_components_Intent(agent){ 
+    if (activecomponents.length === 0){
+        agent.add("currently, there are no active components")     
+    } else {
+        agent.add("currently, active components are " + activecomponents) 
+    }
+}
+
+
+function reset_components_list_Intent(agent){ 
+        activecomponents = []
+        agent.add("Components list are now empty")  
+        console.log(activecomponents)   
+}
+
+
+function Deactivate_component_intent(agent){ 
+        const deactivate = agent.parameters.componentname; 
+        activecomponents.pop(deactivate); 
+        agent.add("succesfully deactivated " + deactivate)        
+}
+
+
+function Activate_component_intent(agent){ 
+        //agent.add("To add the components in order you can say Add and then name of the components")
+        const activate = agent.parameters.activatecomponent; 
+        activecomponents.push(activate); 
+        agent.add("succesfully Added " + activate + " you can add more components by saying Add and then name of the components")       
+}
+
+
 /**
 * now listing the server on port number 3000 :)
 * */
